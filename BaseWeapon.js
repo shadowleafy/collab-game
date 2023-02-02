a3c2f1d88f79ed3d5d4c4b4c33c967361da0ee2d
shadowleafy <81775868+shadowleafy@users.noreply.github.com>
2023-02-02 09:25:18 -0800

    Update BaseWeapon.js

--- BaseWeapon.js
+++ BaseWeapon.js
@@ -1,7 +1,9 @@
 class BaseWeapon{
-    constructor(aName,aDescription,aDamage){
+    constructor(aName,aDescription,aDamage,art,ability){
         this.name = aName
         this.description = aDescription
         this.damage = aDamage
+        this.art = art
+        this.ability = ability
     }
 }
